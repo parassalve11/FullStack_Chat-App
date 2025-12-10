f998bba1b237866ff145bb16c9871828de45d6d5
Paras Salve <160144558+parassalve1@users.noreply.github.com>
2025-12-10 13:43:42 +0530

    style: Improve code formatting and consistency in index.ts

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,37 +1,42 @@
 import express from "express";
-import dotenv from 'dotenv'
-import {createClient} from 'redis'
+import dotenv from "dotenv";
+import { createClient } from "redis";
 import userRoutes from "./routes/user.route.js";
-import {connRabbitMQ} from "./lib/rabbitmq.js";
+import { connRabbitMQ } from "./lib/rabbitmq.js";
 import { connectDb } from "./lib/db.js";
-import cors from 'cors'
-dotenv.config()
+import cors from "cors";
+dotenv.config();
 
-
-
-export const redisClient =  createClient({
-    url:process.env.REDIS_URL
+export const redisClient = createClient({
+  url: process.env.REDIS_URL,
 });
 
-redisClient.connect().then(() => console.log("Redis is connected")).catch(console.error)
+redisClient
+  .connect()
+  .then(() => console.log("Redis is connected"))
+  .catch(console.error);
 
 const app = express();
 
-app.use(cors({
-    origin:"http://13.233.255.160:3000",
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://13.233.255.160",
+      "http://13.233.255.160:3000",
+      "http://localhost:3000",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 
-app.use('/api/v1/users',userRoutes);
-
+app.use("/api/v1/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT , ()  =>{
-    
-    console.log("The server is Running on ", PORT);
-    connectDb();
-    connRabbitMQ();
-})
+app.listen(PORT, () => {
+  console.log("The server is Running on ", PORT);
+  connectDb();
+  connRabbitMQ();
+});

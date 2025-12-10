fe36cfafa3ac5e52c55e134ab9e0929e4c4ae68f
Paras Salve <160144558+parassalve1@users.noreply.github.com>
2025-12-10 15:51:53 +0530

    fix: Update API endpoints to use the correct server address

--- frontend/frontend/src/lib/axios.ts
+++ frontend/frontend/src/lib/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 
 
 export const axiosIntance = axios.create({
-    baseURL:"http://13.233.255.160:5000/api/v1",
+    baseURL:"http://43.204.231.232:5000/api/v1",
     withCredentials:true
 });
